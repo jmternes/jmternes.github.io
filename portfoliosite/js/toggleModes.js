ccea6bcf0a7440bd786cb17153a6eccb08a7ed8b
jmternes <68615887+jmternes@users.noreply.github.com>
2024-07-25 14:04:45 -0500

    more big overhaul things

--- portfoliosite/js/toggleModes.js
+++ portfoliosite/js/toggleModes.js
@@ -2,3 +2,8 @@ function toggleMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
+
+document.querySelector('.my-work').addEventListener('click', function() {
+    document.querySelector('.body-content').style.display = 'none';
+    document.querySelector('.work-content').style.display = 'flex';
+});
